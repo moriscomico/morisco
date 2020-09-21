d467638bc1e30f915f63ab72fc031e361f1784b6
moriscomico <71116931+moriscomico@users.noreply.github.com>
2020-09-21 10:50:37 +0700

    BasicScripting

---
+++ Assets/Latihan1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour {
+
+	void FixedUpdate () {
+		Debug.Log ("Waktu untuk FixedUpdate :" + Time.deltaTime);
+	}
+	void Update () {
+		Debug.Log ("Waktu untuk Update :" + Time.deltaTime);
+	}
+	void LateUpdate () {
+		Debug.Log ("Waktu untuk LateUpdate :" + Time.deltaTime);
+	}
+}
---
+++ Assets/Latihan3.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour {
+
+
+
+	void Start (){
+		int c = jumlah (60, 40);
+		Debug.Log ("Hasil Jumlah a dan b adalah" + c);
+	}
+	void Update (){
+		
+	}
+	int jumlah (int a,int b){
+		return a + b;
+	}
+
+}
