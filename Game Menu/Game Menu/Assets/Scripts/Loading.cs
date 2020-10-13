e7dfe14fef39bc7137a566d07277909707b69362
moriscomico <71116931+moriscomico@users.noreply.github.com>
2020-10-13 16:27:30 +0700

    Game menu (pertemuan ke 5)

---
+++ Game Menu/Game Menu/Assets/Scripts/Loading.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour 
+{
+
+	//waktu delay untuk load scene
+	[SerializeField] private float delayLoading = 3f;
+
+	//nama scene untuk pindah scene yang di inputkan pada inspector
+	[SerializeField] private string namaScene;
+
+	private float timElapsed;
+
+	private void Update () {
+		timElapsed += Time.deltaTime;
+
+		if (timElapsed > delayLoading) {
+			SceneManager.LoadScene (namaScene);
+		}
+	}
+}
---
+++ Game Menu/Game Menu/Assets/Scripts/Scene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+	//pindah scene ke menu
+	public void MenuScene(){
+		SceneManager.LoadScene ("Menu");
+	}
+
+	//pindah scene loading
+	public void LoadingScene(){
+		SceneManager.LoadScene ("Loading");
+	}
+
+	//keluar dari game
+	public void KeluarScene(){
+		Application.Quit ();
+	}
+	// Use this for initialization
+	void Start (){ 
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
